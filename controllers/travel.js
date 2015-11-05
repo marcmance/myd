238b3e483403d5df12148f12d163863c80b7ae23
Mance <marc.mance@gmail.com>
2015-11-05 17:50:51 -0500

    Copy

--- controllers/travel.js
+++ controllers/travel.js
@@ -1,9 +1,15 @@
-myd.controller('travelController', ['$scope', '$rootScope', '$route', '$firebaseObject', '$firebaseArray', function ($scope, $rootScope, $route, $firebaseObject, $firebaseArray) {
+myd.controller('travelController', ['$scope', '$rootScope', '$route', '$firebaseObject', '$firebaseArray', '$window',
+	function ($scope, $rootScope, $route, $firebaseObject, $firebaseArray, $window) {
 
     /*var ref = new Firebase("https://marcandjennyromance.firebaseio.com/person/" + $route.current.params.id);
     $scope.person = $firebaseObject(ref);*/
 
     console.log("travel");
     $rootScope.setCurrentLink(3);
+
+    $scope.goTo = function(url) {
+    	console.log("?");
+    	$window.open(url);
+    }
 
 }]);
